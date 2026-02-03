61077030ebb23f092bc2c7a833eda5373fc08a85
Thomas G. Lopes <thomas.lopes@attio.com>
2026-02-03 11:42:58 +0000

    biome format on save

--- private_dot_config/nvim/lsp/biome.lua
+++ private_dot_config/nvim/lsp/biome.lua
@@ -14,4 +14,12 @@ return {
 		"svelte",
 	},
 	root_markers = { "biome.json", "biome.jsonc" },
+	on_attach = function(client, bufnr)
+		vim.api.nvim_create_autocmd("BufWritePre", {
+			buffer = bufnr,
+			callback = function()
+				vim.lsp.buf.format({ bufnr = bufnr, id = client.id })
+			end,
+		})
+	end,
 }
