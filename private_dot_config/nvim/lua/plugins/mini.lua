8534c65ff67c916d9dd6737ea9c8846846fd4fdd
Thomas G. Lopes <thomas.lopes@attio.com>
2026-02-05 12:43:01 +0000

    snacks renamer

--- private_dot_config/nvim/lua/plugins/mini.lua
+++ private_dot_config/nvim/lua/plugins/mini.lua
@@ -349,5 +349,12 @@ return {
 				width_nofocus = 15,
 			},
 		})
+
+		vim.api.nvim_create_autocmd("User", {
+			pattern = "MiniFilesActionRename",
+			callback = function(event)
+				Snacks.rename.on_rename_file(event.data.from, event.data.to)
+			end,
+		})
 	end,
 }
