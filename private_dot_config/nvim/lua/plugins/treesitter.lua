bf8d73faa33da4ea2d6f64066b8a428172e4f523
Thomas G. Lopes <thomas.lopes@attio.com>
2026-02-10 11:38:58 +0000

    nvim settings

--- private_dot_config/nvim/lua/plugins/treesitter.lua
+++ private_dot_config/nvim/lua/plugins/treesitter.lua
@@ -1,59 +1,39 @@
 return {
 	-- Highlight, edit, and navigate code
 	"nvim-treesitter/nvim-treesitter",
+	lazy = false,
 	build = ":TSUpdate",
-	main = "nvim-treesitter.configs", -- Sets main module to use for opts
-	-- [[ Configure Treesitter ]] See `:help nvim-treesitter`
 	config = function()
 		require("nvim-treesitter").setup({
-			-- A list of parser names, or "all"
-			ensure_installed = {
-				"vimdoc",
-				"javascript",
-				"typescript",
-				"c",
-				"lua",
-				"rust",
-				"jsdoc",
-				"bash",
-				"svelte",
-				"astro",
-				"vue",
-				"css",
-				"scss",
-				"gdscript",
-			},
-
-			-- Install parsers synchronously (only applied to `ensure_installed`)
-			sync_install = false,
-
-			-- Automatically install missing parsers when entering buffer
-			-- Recommendation: set to false if you don"t have `tree-sitter` CLI installed locally
-			auto_install = true,
-
-			indent = {
-				enable = true,
-				disable = { "gdscript" },
-			},
-
-			highlight = {
-				-- `false` will disable the whole extension
-				enable = true,
-
-				-- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-				-- Set this to `true` if you depend on "syntax" being enabled (like for indentation).
-				-- Using this option may slow down your editor, and you may see some duplicate highlights.
-				-- Instead of true it can also be a list of languages
-				additional_vim_regex_highlighting = { "markdown" },
-			},
+			install_dir = vim.fn.stdpath("data") .. "/site",
 		})
 
-		-- vim.filetype.add({
-		-- 	extension = {
-		-- 		mdx = "mdx",
-		-- 	},
-		-- })
+		-- Install parsers (async, no-op if already installed)
+		require("nvim-treesitter").install({
+			"vimdoc",
+			"javascript",
+			"typescript",
+			"tsx",
+			"ripple",
+			"c",
+			"lua",
+			"rust",
+			"jsdoc",
+			"bash",
+			"svelte",
+			"astro",
+			"vue",
+			"css",
+			"scss",
+			"gdscript",
+		})
 
-		-- vim.treesitter.language.register("markdown", "mdx")
+		-- Enable treesitter highlighting and indentation
+		vim.api.nvim_create_autocmd("FileType", {
+			callback = function()
+				pcall(vim.treesitter.start)
+				vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+			end,
+		})
 	end,
 }
